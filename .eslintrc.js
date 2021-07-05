ac35bdab31ad4451126244cd6d0a2d2ddbfd3969
milkmidi <milkmidi.liao@positivegrid.com>
2021-07-05 23:09:14 +0800

    feat: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,15 @@
+module.exports = {
+  extends: [
+    'airbnb',
+  ],
+  env: {
+    browser: true,
+    jest: true,
+  },
+  parser: 'babel-eslint',
+  settings: {
+  },
+  rules: {
+    'global-require': 0,
+  },
+};
